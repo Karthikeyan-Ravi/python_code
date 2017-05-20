3d62cade15627e8173fc272b4ca566b8d7662a12
Karthikeyan-Ravi <karthikeyan.ravi11@gmail.com>
2017-05-20 08:27:04 +0530

    new change
    changes in print statement

--- adding_two_arguments.py
+++ adding_two_arguments.py
@@ -11,7 +11,7 @@ def add(arg1,arg2):
 
 if __name__=='__main__':
     #print "argument1 ="+str(arg1) + "argument2 ="+str(arg2)
-    print "adding two arguments"
+    print "adding two arguments of any data type"
     result = add(5,6)
     print result
     result = add("ashok ","marannan")
